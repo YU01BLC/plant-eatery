a4d086ff200b398a9609d832c13085d7012dae09
YU01BLC <bgc00magenta@gmail.com>
2023-08-07 00:01:01 +0900

    add: 管理画面のベースとなるダミーページを作成

---
+++ src/page/admin.tsx
@@ -0,0 +1,11 @@
+import { Box, Text, VStack } from '@chakra-ui/react';
+
+export default function Admin() {
+  return (
+    <VStack>
+      <Box>
+        <Text>Admin view</Text>
+      </Box>
+    </VStack>
+  );
+}
