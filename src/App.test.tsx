da36cdba815fc24af2201ac70ea8d061efdcd1bf
YU01BLC <bgc00magenta@gmail.com>
2023-08-07 00:03:01 +0900

    update: テストケースを更新

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('Textタグにラップされている文言「Hello World」の表示確認', () => {
-  render(<App />);
-  const textElement = screen.getByText('Hello World');
-  expect(textElement).toBeInTheDocument();
+test('パス「/admin」アクセス時に`Adminコンポーネント`が表示されること', () => {
+  // テスト用のメモリルーターを作成し、初期エントリーとして'/admin'を設定する
+  render(
+    <MemoryRouter initialEntries={['/admin']}>
+      <App />
+    </MemoryRouter>
+  );
+});
+
+test('パス「/Admin」以外のアクセス時に`Indexコンポーネント`が表示されること', () => {
+  // テスト用のメモリルーターを作成し、初期エントリーとして'/random-path'を設定する
+  render(
+    <MemoryRouter initialEntries={['/random-path']}>
+      <App />
+    </MemoryRouter>
+  );
 });
