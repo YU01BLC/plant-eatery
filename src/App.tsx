c3b66efc6f58178dfe01052f1f05538fb4d3a019
YU01BLC <bgc00magenta@gmail.com>
2023-08-06 18:19:26 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Text, VStack } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <VStack>
+      <Box>
+        <Text>Hello World</Text>
+      </Box>
+    </VStack>
+  );
+}
+
+export default App;
