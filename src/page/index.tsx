05f41bc2775a52d53ddbd233b97152fe4bbcfd12
YU01BLC <bgc00magenta@gmail.com>
2023-08-07 00:01:22 +0900

    add: ユーザ用画面のベースとなるダミーページを作成

---
+++ src/page/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Text, VStack } from '@chakra-ui/react';
+
+export default function Index() {
+  return (
+    <VStack>
+      <Box>
+        <Text>User view</Text>
+      </Box>
+    </VStack>
+  );
+}
