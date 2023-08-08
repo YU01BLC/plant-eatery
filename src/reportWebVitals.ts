a6ac2693f598aec6f6d9a90406ba327276c81119
YU01BLC <bgc00magenta@gmail.com>
2023-08-08 23:18:52 +0900

    update: lintの制約に合わせて修正

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,16 @@
-import { ReportHandler } from 'web-vitals';
+import { ReportHandler, getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    try {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
-    });
+    } catch (error) {
+      console.error('web-vitals import failed', error);
+    }
   }
 };
 
