00532a5bf16375730130a38c8913b4d51ac5c95c
YU01BLC <bgc00magenta@gmail.com>
2023-08-08 23:16:37 +0900

    clean: functionの形を更新

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import Admin from './page/admin';
 import Index from './page/index';
 
-const App = () => {
+export default function App() {
   return (
     <Routes>
       <Route path='/admin' element={<Admin />} />
       <Route path='*' element={<Index />} />
     </Routes>
   );
-};
-
-export default App;
+}
