61d39dcb4e0916f5610189f5f55b37b75004c867
YU01BLC <bgc00magenta@gmail.com>
2023-08-14 23:53:41 +0900

    delete: テストケースを削除

--- src/__tests__/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Index from '../page/index';
-
-test('Textタグにラップされている文言「User view」の表示確認', () => {
-  render(<Index />);
-  const textElement = screen.getByText(/User view/iu);
-  expect(textElement).toBeInTheDocument();
-});
