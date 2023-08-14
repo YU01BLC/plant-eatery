53c0418eaa010d7c286279b6ad74a68fa17b4076
YU01BLC <bgc00magenta@gmail.com>
2023-08-14 23:52:44 +0900

    add: amplify認証関連ファイル・設定を追加

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "planteaterybf8453aa": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
