3a579c7d80bc8801bdae36823eb3d3cb2589da36
YU01BLC <bgc00magenta@gmail.com>
2023-08-22 17:18:20 +0900

    add: AWSリソースにアクセスする情報を参照する用ファイルを作成

---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
+  region: process.env.REACT_APP_REGION,
+  bucketName: process.env.REACT_APP_S3_BUCKET_NAME,
+};
