ec885a0df0824cd54972f3f1a9cdf5eb08950e68
YU01BLC <bgc00magenta@gmail.com>
2024-03-07 09:29:55 +0900

    fix: 型チェックをする必要がない箇所をスキップするよう更新

--- app/utils/ConfigureAmplifyClientSide.ts
+++ app/utils/ConfigureAmplifyClientSide.ts
@@ -3,6 +3,7 @@
 import { Amplify } from 'aws-amplify';
 import config from '../src/amplifyconfiguration.json';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 Amplify.configure(config, { ssr: true });
 
 export default function ConfigureAmplifyClientSide() {
